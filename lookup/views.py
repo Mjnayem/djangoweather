659c090227b943fab95bc1c7922f1dd30ed9d5df
mjnayem <mjnayem@gmail.com>
2021-02-23 20:45:41 +0600

    updated

--- lookup/views.py
+++ lookup/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 
-
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+
+    if request.method == "POST":
+        zipcode = request.POST['zipcode']
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+zipcode+"&distance=25&API_KEY=B61ABBBE-EFDE-4DED-BC33-B9C29B956E38")
+        
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error found"
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=B61ABBBE-EFDE-4DED-BC33-B9C29B956E38")
+        
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error found"
+
+        return render(request, 'home.html', {'api': api})
 
 
 def about(request):
